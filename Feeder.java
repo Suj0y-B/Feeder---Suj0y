5bb43023b7209ad99552719fead472716119d9c4
Suj0y-B <sujoyb@nycstudents.net>
2024-10-16 16:35:48 +0000

    f

--- Feeder.java
+++ Feeder.java
@@ -5,15 +5,23 @@ public class Feeder {
      * The amount of food, in grams, currently in the bird feeder; initialized
      * in the constructor and always greater than or equal to zero
      */
-    int currentFood;
+    private int currentFood;
 
     /**
      * Simulates one day with numBirds birds or possibly a bear at the bird
      * feeder, as described in part (a) Precondition: numBirds > 0
      */
-    public void simulateOneDay(int numBirds)
-    {
-        
+    public void simulateOneDay(int numBirds) {
+
+        if (Math.random() < 0.05)
+            currentFood = 0;
+        else {
+            int eaten = (int) (Math.random() * 41) + 10;
+            currentFood -= eaten * numBirds;
+            if (currentFood < 0)
+                currentFood = 0;
+        }
+
     }
 
     /**
@@ -21,21 +29,33 @@ public class Feeder {
      * feeder in this simulation, as described in part (b) Preconditions:
      * numBirds > 0, numDays > 0
      */
-    public int simulateManyDays(int numBirds, int numDays) 
-    {
+    public int simulateManyDays(int numBirds, int numDays) {
+        int days = 0;
+        while(numDays > 0)
+        {
+            if(currentFood == 0) return days;
+            return days;
+        }
+        simulateOneDay(numBirds);
+        numDays--;
+        days++;
         return 0;
     }
 
-
-public int getCurrentFood()
-     {
-    return currentFood;
-     }
-public Feeder(int c)
-     {
-    currentFood = c;
-     }
-     public Feeder() {}
-
-
+    public int getCurrentFood() {
+        return currentFood;
     }
+
+    public Feeder(int c) {
+        currentFood = c;
+    }
+
+    public Feeder() {
+    }
+    public String toString()
+    {
+        return "CurrentFood: " + currentFood;
+        
+    }
+    
+}
--- Main.java
+++ Main.java
@@ -4,11 +4,26 @@ public class Main
     {
         Feeder f = new Feeder();
         System.out.println(f);
-        System.out.println(f.currentFood);
+        //System.out.println(f.currentFood);
         //system.out.println(f.currentFood);
         System.out.println(f.getCurrentFood());
         Feeder g = new Feeder (500);
         System.out.println(g.getCurrentFood());
-        
+        System.out.println(Math.random() < 0.05);
+        int random = (int)(Math.random() * 10 + 1);
+        System.out.println(random);
+        g.simulateOneDay(12);
+        System.out.println(g.getCurrentFood());
+        Feeder h = new Feeder(1000);
+        h.simulateOneDay(22);
+        System.out.println(h.getCurrentFood());
+        Feeder i = new Feeder(100);
+        i.simulateOneDay(5);
+        System.out.println(i);
+        Feeder j = new Feeder(2400);
+        System.out.println(j.simulateManyDays(10, 4));
+        Feeder k = new Feeder(0);
+        System.out.println(k.simulateManyDays(5, 10));
+
     }
 }
